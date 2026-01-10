25c73f65c79de3661d6f44a6f995309b7ee5d1a4
Abdullah Kaisar Fardin <kaisarfardin128@gmail.com>
2026-01-11 05:54:37 +0600

    pass change added

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import (
     RegisterView, VerifyOTPView, LoginView, LogoutView, 
     ResendOTPView, ForgotPasswordView, ResetPasswordConfirmView, 
-    UserProfileView, GoogleLoginView, AppleLoginView
+    UserProfileView, GoogleLoginView, AppleLoginView,ChangePasswordView  
 )
 
 urlpatterns = [
@@ -16,4 +16,5 @@ urlpatterns = [
     path('profile/', UserProfileView.as_view(), name='user-profile'),
     path('google/', GoogleLoginView.as_view(), name='google_login'),
     path('apple/', AppleLoginView.as_view(), name='apple_login'),
+    path('change-password/', ChangePasswordView.as_view(), name='change-password'),
 ]
